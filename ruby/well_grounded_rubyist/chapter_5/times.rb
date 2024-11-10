7caf6841659ba13d69f061c380985978d5297335
Roman Andreeff <di0niixs@gmail.com>
2024-11-10 20:22:35 +0300

    Update times.rb

--- ruby/well_grounded_rubyist/chapter_5/times.rb
+++ ruby/well_grounded_rubyist/chapter_5/times.rb
@@ -7,8 +7,21 @@ class Integer
       yield(i)
       i += 1
     end
-    i
+    self
   end
 end
 
 5.my_times { |i| puts "I'm on iteration #{i}" }
+
+# class Integer
+#   def my_times
+#     i = 0
+#     until i == self
+#       yield(i)
+#       i += 1
+#     end
+#     self
+#   end
+# end
+#
+# 5.my_times { |i| puts "I'm on iteration #{i}" }
